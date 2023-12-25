8f3830a153295dfa63eab301a8097f2443a5c6d9
WinFan3672 <>
2023-12-25 20:14:46 +0000

    Disabled `/` option (for now)

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from textual.containers import Horizontal
 
 STARTDIR = os.path.expanduser("~")
 
+DIRS = [STARTDIR, STARTDIR]
+
 class LeftPanel(Static):
     def compose(self) -> ComposeResult:
         yield Label("This is pre-release software; expect missing features and bugs.")
@@ -12,12 +14,15 @@ class FilterBox(Static):
     def compose(self) -> ComposeResult:
         yield Input("Filter...", id="filterBox")
         yield Button("Go", id="goButton")
+class DirTrees(Static):
+    def compose(self) -> ComposeResult:
+        yield Horizontal(DirectoryTree(STARTDIR), DirectoryTree(STARTDIR))
 class MainApp(App):
     TITLE = "File Browser"
     '''A file explorer written in Textual.'''
     CSS_PATH = "app.css"
     BINDINGS = [
-            ('/', 'filter', 'Filter'),
+            # ('/', 'filter', 'Filter'),
             ('?', 'help', 'Help'),
             ('q', 'quit', 'Quit'),
             ]
@@ -26,8 +31,7 @@ class MainApp(App):
         yield Header()
         yield Footer()
         yield LeftPanel()
-        yield Horizontal(DirectoryTree(STARTDIR), DirectoryTree(STARTDIR))
-
+        yield DirTrees()
     def action_quit(self) -> None:
         self.exit()
     
