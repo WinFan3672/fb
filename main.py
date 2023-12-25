fdeb44f2f619bea469268c3517864d054fbd2f07
WinFan3672 <>
2023-12-25 21:10:01 +0000

    Created address bars

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
 import os
 from textual.app import App, ComposeResult
 from textual.widgets import Button, Header, Footer, Static, DirectoryTree, Label, Input
-from textual.containers import Horizontal
+from textual.containers import Horizontal, Vertical
+from textual.reactive import reactive
 
 STARTDIR = os.path.expanduser("~")
 
 DIRS = [STARTDIR, STARTDIR]
 
+class DirLabel(Label):
+    pass
 class LeftPanel(Static):
+    pass
+class WarningBox(Static):
     def compose(self) -> ComposeResult:
         yield Label("This is pre-release software; expect missing features and bugs.")
 class FilterBox(Static):
     def compose(self) -> ComposeResult:
         yield Input("Filter...", id="filterBox")
         yield Button("Go", id="goButton")
+class DirTree(DirectoryTree):
+    show_root = reactive(False)
 class DirTrees(Static):
     def compose(self) -> ComposeResult:
-        yield Horizontal(DirectoryTree(STARTDIR), DirectoryTree(STARTDIR))
+        yield Horizontal(Vertical(DirLabel(DIRS[0]), DirTree(DIRS[0])), Vertical(DirLabel(DIRS[0]), DirTree(DIRS[1])))
 class MainApp(App):
     TITLE = "File Browser"
     '''A file explorer written in Textual.'''
     CSS_PATH = "app.css"
     BINDINGS = [
             # ('/', 'filter', 'Filter'),
-            ('?', 'help', 'Help'),
+            # ('?', 'help', 'Help'),
             ('q', 'quit', 'Quit'),
             ]
     
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
-        yield LeftPanel()
+        yield WarningBox()
         yield DirTrees()
     def action_quit(self) -> None:
         self.exit()
