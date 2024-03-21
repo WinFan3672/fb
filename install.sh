17e6806f0a7ec701d9b7ea634777773bd99c272e
Gordinator <gordinator@gordinator.org>
2024-03-21 22:47:59 +0000

    Updated install script
    The install script now overrides /tmp/fb if it exists, to ensure that
    the repo is pulled regardless of whether or not it has already been
    pulled.
    
    The install script also installs itself to /bin/fb_update

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@ REPO="https://codeberg.org/WinFan3672/fb.git"
 TEMPDIR="/tmp/fb"
 DATADIR="/usr/share/fb"
 BINFILE="/bin/fb"
+UPDATEFILE="/bin/fb_update"
 
 main() {
     echo "Cloning repo..."
     ## Clone the repo
+    rm -r /tmp/fb || sleep 0
     git clone "$REPO" "$TEMPDIR"
     cd "$TEMPDIR" || exit 1
     ## Copy files
@@ -15,7 +17,9 @@ main() {
     mkdir "$DATADIR" || sleep 0
     cp app.css "$DATADIR/app.css"
     cp main.py "$BINFILE"
+    cp install.sh "$UPDATEFILE"
     chmod +x "$BINFILE"
+    chmod +x "$UPDATEFILE"
     echo "Installation complete! Run fb to see if it works!"
 }
 
